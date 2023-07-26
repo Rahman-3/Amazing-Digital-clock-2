7998a9f962aa95736e413fa117a40f7d6bfb9120
Rahman-3 <139842058+Rahman-3@users.noreply.github.com>
2023-07-26 08:59:13 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startClock(){
+vai todayDate=new Date()
+document.getElementById("dateContainer").innerText=todayDate
+}
+
+function stopClock(){
+
+}
+function show time(){
+    vai todayDate=newDate()
+    vai hours=todayDate.getHours90//0-23
+    vai minutes=todayDate.getMinutes//0-59
+    vai seconds=todayDate.getseconds//0-59
+    vai session='AM'
+    
+
+}
